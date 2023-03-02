4d813c7800cfeaa8855abd6edc6c01feb342a609
jukha <junaid.apsc@gmail.com>
2023-03-02 14:36:55 +0500

    Add new files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { JobDetailsComponent } from './components/job-details/job-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    JobDetailsComponent
   ],
   imports: [
     BrowserModule,
